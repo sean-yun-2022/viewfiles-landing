a6043b7dc4bf63f10a0426a9583d8c6636ba2e03
sean-gaus <sean.us.ga@gmail.com>
2026-06-06 17:21:55 -0400

    fix: update download URLs to v0.2.8

--- app/download/mac/route.ts
+++ app/download/mac/route.ts
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 
-const LATEST_MAC = 'https://github.com/sean-yun-2022/viewfiles-releases/releases/download/v0.2.7/ViewFiles-0.2.6-arm64.dmg'
+const LATEST_MAC = 'https://github.com/sean-yun-2022/viewfiles-releases/releases/download/v0.2.8/ViewFiles-0.2.8-arm64.dmg'
 
 export function GET() {
   redirect(LATEST_MAC)
--- app/download/windows/route.ts
+++ app/download/windows/route.ts
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 
-const LATEST_WINDOWS = 'https://github.com/sean-yun-2022/viewfiles-releases/releases/download/v0.2.7/ViewFiles.Setup.0.2.6.exe'
+const LATEST_WINDOWS = 'https://github.com/sean-yun-2022/viewfiles-releases/releases/download/v0.2.8/ViewFiles.Setup.0.2.8.exe'
 
 export function GET() {
   redirect(LATEST_WINDOWS)
